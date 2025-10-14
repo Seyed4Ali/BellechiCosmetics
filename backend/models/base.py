b486fd3ac6891d30ac6d33c72ea319513e5aec75
Seyed4Ali <ali21.5mousavi@gmail.com>
2025-10-14 22:19:18 +0330

    Ignore docs folder

--- backend/models/base.py
+++
@@ -1,9 +0,0 @@
-# backend/models/base.py
-from sqlmodel import SQLModel, create_engine
-import os
-
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@db:5432/bellechi")
-engine = create_engine(DATABASE_URL, echo=False)
-
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
